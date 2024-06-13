9001259dcd815604725af1a7767b4a1ddc320c2f
Yurii Panchuk <y.panchuk@dev-branch.com>
2024-06-13 10:53:28 -0500

    feat: finalized app.js for server part, improved bot.js file and added each command.

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
+// Bot include.
+const bot = require("./src/bots/bot.js");
 
-const bot = require('./src/bot.js');
+// Init & run express app.
+const express = require("express");
+const app = express();
+
+// Init Middleware.
+app.use(express.json());
+
+// Init & ren database connection.
+const database = require("./src/database/databaseManager.js");
+database.connect();
+
+// Define Routes.
+app.use("/api/suggestions", require("./routes/suggestionsRoute"));
+app.use("/api/complaints", require("./routes/complaintsRoute"));
+app.use("/api/logs", require("./routes/logsRoute"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
--- src/bots/bot.js
+++ src/bots/bot.js
@@ -1,32 +1,89 @@
+// Base includes.
+const { Telegraf, Scenes, session } = require("telegraf");
 
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
-const base = require('./baseMessages')
-const advanced = require('./advancedMessages')
+const appRoot = require("app-root-path");
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, `${appRoot}/.env`) });
 
-const path = require('path')
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+// Additional includes.
+const base = require(`${appRoot}/src/messages/baseMessages`);
+const advanced = require(`${appRoot}/src/messages/advancedMessages`);
 
 // Define bot using telegraf.
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// Base commands.
-bot.command('start', context => base.replyOnStart(context))
-bot.command('help', context => base.replyOnHelp(context))
-bot.command('about', context => base.replyOnAbout(context))
-bot.command('cat', context => base.showCat(context))
+/**
+ * The base command to start bot.
+ *
+ * Calls replyOnStart method that returns:
+ * - Greeting message.
+ * - List of commands.
+ * - Some additional text.
+ *
+ * To the message attached random image with cat.
+ */
+bot.command("start", (context) => base.replyOnStart(context));
 
-// Advanced commands.
-bot.command('suggest', context => advanced.handleSuggestion(context))
-bot.command('complain', context => advanced.handleComplain(context))
+/**
+ * The base command to view the list of commands.
+ *
+ * List of commands contains each available command except estereggs commands.
+ */
+bot.command("help", (context) => base.replyOnHelp(context));
 
+/**
+ * The custom command to get more info about bot.
+ *
+ * Returns text that describes the bot.
+ */
+bot.command("about", (context) => base.replyOnAbout(context));
 
-bot.launch()
+/**
+ * The custom command to get cat picture.
+ *
+ * Funny command that returns random picture of some cat's.
+ */
+bot.command("cat", (context) => base.showCat(context));
+
+// Define a list of scenes.
+const stage = new Scenes.Stage([
+  advanced.handleSuggestion(),
+  advanced.handleComplaint(),
+]);
+
+// Enable sessions and stage middleware.
+bot.use(session());
+bot.use(stage.middleware());
+
+/**
+ * The custom command to leave suggestion.
+ *
+ * Advanced command that provides a few steps flow to leave suggestion.
+ */
+bot.command("suggest", (context) => {
+  context.scene.enter(advanced.SUGGESTION_SCENE).then();
+});
+
+/**
+ * The custom command to leave complain.
+ *
+ * Advanced command that provides a few steps flow to leave complain.
+ */
+bot.command("complain", (context) => {
+  context.scene.enter(advanced.COMPLAINT_SCENE).then();
+});
+
+/**
+ * Try to launch bot and handle result.
+ */
+bot
+  .launch()
+  .then(() => console.log("Bip-Bop.. Bot started..."))
+  .catch((error) => {
+    console.log("Bip-Boop.. Something went wrong..");
+    console.log("Error:", error);
+  });
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
-
-
-
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
