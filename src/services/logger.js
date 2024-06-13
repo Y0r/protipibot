8ccc84c38e16d4ab5eb8db9d6ff57d1c6352804e
Yurii Panchuk <y.panchuk@dev-branch.com>
2024-06-13 10:54:42 -0500

    feat: finalized logger service, enabled logging.

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,14 +1,18 @@
-
-const path = require('path');
-const yaml= require('js-yaml');
-const fs= require('fs');
+const axios = require("axios");
+const appRoot = require("app-root-path");
+const path = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 /**
  * Path to the texting config file.
  *
  * @type {string}
  */
-const LOGGER_CONFIG_PATH = path.resolve(__filename, '../../config/logger.yml');
+const LOGGER_CONFIG_PATH = path.resolve(
+  __filename,
+  `${appRoot}/config/logger.yml`,
+);
 
 /**
  * Log message/command usage.
@@ -19,13 +23,46 @@ const LOGGER_CONFIG_PATH = path.resolve(__filename, '../../config/logger.yml');
  *   Context object.
  */
 function log(type, context) {
-  if (isLoggerEnabled()) {
+  // Check if logging enabled.
+  if (!isLoggerEnabled()) {
     return;
   }
 
   let from = userString(context);
-  console.log(`< ${type} <<`, context.message.text, from)
+  console.log(`< ${type} <<`, context.message.text, from);
+
+  // Check if database logging enabled.
+  if (!isLoggerEnabled("db_log")) {
+    return;
+  }
+
+  createLog(type, context).then((r) => console.log("Log saved in MongoDB."));
 }
+
+/**
+ * Creates a log record.
+ *
+ * @param type
+ *   Log type.
+ * @param context
+ *   Given context object.
+ * @returns {Promise<void>}
+ *   Promise of request.
+ */
+const createLog = async (type, context) => {
+  try {
+    const response = await axios.post("http://localhost:5000/api/logs", {
+      message: JSON.stringify(context.message),
+      level: type,
+      date: Date.now(),
+    });
+  } catch (error) {
+    console.error(
+      "Error registering user:",
+      error.response ? error.response.data : error.message,
+    );
+  }
+};
 
 /**
  * Stringify content data.
@@ -37,9 +74,13 @@ function log(type, context) {
  *   String from message.
  */
 function userString(context) {
-  return JSON.stringify(context.from.id === context.chat.id ? context.from : {
+  if (context.from.id === context.chat.id) {
+    return JSON.stringify({ from: context.from });
+  }
+
+  return JSON.stringify({
     from: context.from,
-    chat: context.chat
+    chat: context.chat,
   });
 }
 
@@ -48,13 +89,13 @@ function userString(context) {
  *
  * @returns {*}
  */
-function isLoggerEnabled() {
+function isLoggerEnabled(key = "enabled") {
   // Load config with settings.
-  const config = yaml.load(fs.readFileSync(LOGGER_CONFIG_PATH, 'utf8'));
-  const settings = config['settings'];
-  return settings['enabled'];
+  const config = yaml.load(fs.readFileSync(LOGGER_CONFIG_PATH, "utf8"));
+  const settings = config["settings"];
+  return settings[key];
 }
 
 module.exports = {
-  log
-}
+  log,
+};
